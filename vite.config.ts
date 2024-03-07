083cb8924fc834eff76f46c7445879ea7432a3c1
Ningaro <23503775+ningaro@users.noreply.github.com>
2024-03-07 18:24:07 +0300

    🚀 Deploy to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: mode === "production" ? "https://ctulhu-vpn.github.io/gh-test/" : "/",
 }))
